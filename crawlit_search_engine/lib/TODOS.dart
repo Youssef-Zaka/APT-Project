ae0c96777210234de44541339c2a9fea846c5e09
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2022-05-13 09:51:13 +0200

    updated todos

--- crawlit_search_engine/lib/TODOS.dart
+++ crawlit_search_engine/lib/TODOS.dart
@@ -1,10 +1,12 @@
 /*
 TODO: 
-- Implement Recents 
-- Implement Suggestions (depends on backend)
+- Implement Recents (REMOVED NO TIME TO IMPLEMENT)
+- Implement Suggestions 
+- Highlight snippets of where the search phrase was found
 - Integrate real Search results instead of hard coded list
 - add pagination
 - add loading blocks when fetching data
+- Connect to backend using Sockets
 - add voice recogoition but fr no cap
 - make it pretty
 */
